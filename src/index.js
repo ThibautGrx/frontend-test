a7887b21fcd744cd16cdd2d749e11c793a5255b9
Thibaut Gorioux <gorioux.thibaut@gmail.com>
2019-07-08 15:23:59 +0200

    level 1

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import "./index.css";
+import CarList from './carList'
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(<CarList />, document.getElementById('root'));
